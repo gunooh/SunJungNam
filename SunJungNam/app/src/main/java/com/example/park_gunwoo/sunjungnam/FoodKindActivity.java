383f95ebb19b4d0f6c1d146125ab813437c1e78a
gunooh <gunooh12@gmail.com>
2017-05-28 01:40:27 +0900

    sunjungnam

--- SunJungNam/app/src/main/java/com/example/park_gunwoo/sunjungnam/FoodKindActivity.java
+++ SunJungNam/app/src/main/java/com/example/park_gunwoo/sunjungnam/FoodKindActivity.java
@@ -1,7 +1,9 @@
 package com.example.park_gunwoo.sunjungnam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FoodKindActivity extends AppCompatActivity {
 
@@ -9,5 +11,41 @@ public class FoodKindActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_kind);
+    }
+
+
+    public void onClickKoreaFood(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), KoreaFoodSelectActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickWesternFood(View v)
+    {
+//        Intent intent = new Intent(getApplicationContext(), WesternFoodSelectActivity.class);
+//        startActivity(intent);
+    }
+
+    public void onClickChineseFood(View v)
+    {
+//        Intent intent = new Intent(getApplicationContext(), ChineseFoodSelectActivity.class);
+//        startActivity(intent);
+    }
+
+    public void onClickEtc(View v)
+    {
+//        Intent intent = new Intent(getApplicationContext(), EtcSelectActivity.class);
+//        startActivity(intent);
+    }
+
+    public void onClickBack(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickRandom(View v)
+    {
+
     }
 }
