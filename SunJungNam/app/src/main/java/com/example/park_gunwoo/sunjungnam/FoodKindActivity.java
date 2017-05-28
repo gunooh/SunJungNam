6eb635ad0a1d61c6a8b4b4559905401335cde8c1
gunooh <gunooh12@gmail.com>
2017-05-29 02:14:56 +0900

    SunJungNam

--- SunJungNam/app/src/main/java/com/example/park_gunwoo/sunjungnam/FoodKindActivity.java
+++ SunJungNam/app/src/main/java/com/example/park_gunwoo/sunjungnam/FoodKindActivity.java
@@ -7,6 +7,14 @@ import android.view.View;
 
 public class FoodKindActivity extends AppCompatActivity {
 
+    Class[] FoodArray = {Sullungtang.class, Gukbap.class, Bibimbap.class, Dupbap.class,
+                        Naengmyun.class, Kalguksu.class, Bulgogi.class, Galbijjim.class,
+                        Jjambbong.class, Jajangmyun.class, Ulmyun.class,
+                        Bokumbap.class, Japchaebap.class, Tangsuyuk.class, Palbochae.class,
+                        Steak.class, Dongas.class, Pasta.class, Spagetti.class,
+                        Udong.class, Ramen.class, Chobap.class, Sushi.class,
+                        Gimbap.class, Ramyun.class, Jjolmyun.class,
+                        Ddukbboki.class, Sundae.class, Tuigim.class};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,20 +30,20 @@ public class FoodKindActivity extends AppCompatActivity {
 
     public void onClickWesternFood(View v)
     {
-//        Intent intent = new Intent(getApplicationContext(), WesternFoodSelectActivity.class);
-//        startActivity(intent);
+        Intent intent = new Intent(getApplicationContext(), WesternFoodSelectActivity.class);
+        startActivity(intent);
     }
 
     public void onClickChineseFood(View v)
     {
-//        Intent intent = new Intent(getApplicationContext(), ChineseFoodSelectActivity.class);
-//        startActivity(intent);
+        Intent intent = new Intent(getApplicationContext(), ChineseFoodSelectActivity.class);
+        startActivity(intent);
     }
 
     public void onClickEtc(View v)
     {
-//        Intent intent = new Intent(getApplicationContext(), EtcSelectActivity.class);
-//        startActivity(intent);
+        Intent intent = new Intent(getApplicationContext(), EtcSelectActivity.class);
+        startActivity(intent);
     }
 
     public void onClickBack(View v)
@@ -46,6 +54,8 @@ public class FoodKindActivity extends AppCompatActivity {
 
     public void onClickRandom(View v)
     {
-
+        int ranNum = (int)(Math.random()*29);
+        Intent intent = new Intent(getApplicationContext(), FoodArray[ranNum]);
+        startActivity(intent);
     }
 }
